f56ec0ccfdfcab9fc0a2c1eeb14897f5600b603c
Vitalii_H <hryhorenko.vitalii@gmail.com>
2021-10-15 15:21:59 -0400

    add new variable to settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,22 @@
-##Scraper settings
+##Database settings
 
 #setting psql
-PSQL_CREDENTIAL = ""
+PSQL_CREDENTIAL = 'postgresql://postgres:sacred@localhost:5432/postgres'
 PSQL_TABLE_NAME = ""
 PSQL_TABLE_STRUCTURE = ""
+
+
+##Scraper settings
 
 #setting request
 REQUEST_ATTEMPTS:int = 10
 REQUEST_HEADERS:str  = None
 REQUEST_TIMEOUT:int = 10
 PROXY:str = None
+
+
+##Other settings
+DEBUG_LEVEL:str = "DEBUG"
+
+##Api settings
+X_API_KEY = {"Main password": "password"}
